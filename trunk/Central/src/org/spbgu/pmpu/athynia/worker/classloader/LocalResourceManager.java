6e2041b9d06bd8cd1f904f3875e6af7550fdd3f4
VassiliPisar <VassiliPisar@a7eabe5b-282f-0410-88e4-01b19cccb18e>
2007-04-29 19:46:42 +0000

    LocalResourceManager fixed

--- trunk/Central/src/org/spbgu/pmpu/athynia/worker/classloader/LocalResourceManager.java
+++ trunk/Central/src/org/spbgu/pmpu/athynia/worker/classloader/LocalResourceManager.java
@@ -1,7 +1,42 @@
 package org.spbgu.pmpu.athynia.worker.classloader;
 
+import org.spbgu.pmpu.athynia.central.communications.join.JoinPart;
+import org.spbgu.pmpu.athynia.central.communications.join.impl.JoinPartImpl;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * User: vasiliy
  */
 public class LocalResourceManager {
+    private long previousDropTime;
+    private JoinPart previousJoinPart;
+
+    private Map<String, JoinPart> index = new HashMap<String, JoinPart>();
+
+    public void write(String key, String value, int currentPartNumber, int wholeNumbers, long timeoutUntilDrop) {
+        previousDropTime = System.currentTimeMillis() + timeoutUntilDrop;
+        previousJoinPart = new JoinPartImpl(key, value, currentPartNumber, wholeNumbers);
+    }
+
+    public void commit() {
+        if (System.currentTimeMillis() <= previousDropTime) {
+            index.put(previousJoinPart.getKey(), previousJoinPart);
+        }
+    }
+
+    public void abort() {
+        previousDropTime = 0;
+        previousJoinPart = null;
+    }
+
+    public JoinPart search(String key) {
+        JoinPart ret = index.get(key);
+        if (ret == null) {
+            return new JoinPartImpl(key, null, 0, -1);
+        } else {
+            return ret;
+        }
+    }
 }
