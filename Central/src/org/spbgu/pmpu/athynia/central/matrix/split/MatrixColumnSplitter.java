6274519b62d62c3026eae493105cb88c0d8687c3
A.Selivanov <A.Selivanov@a7eabe5b-282f-0410-88e4-01b19cccb18e>
2007-06-16 22:00:48 +0000

    splitter perfomance fix

--- Central/src/org/spbgu/pmpu/athynia/central/matrix/split/MatrixColumnSplitter.java
+++ Central/src/org/spbgu/pmpu/athynia/central/matrix/split/MatrixColumnSplitter.java
@@ -9,16 +9,18 @@ import java.util.Arrays;
  * User: A.Selivanov
  * Date: 08.06.2007
  */
-public class MatrixColumnSplitter <Value extends Matrix> implements DataSplitter<Value> {
+public class MatrixColumnSplitter<Value extends Matrix> implements DataSplitter<Value> {
     public String[] splitData(Value matrix, int parts) {
         int size = matrix.size();
         String[] result = new String[parts];
         Arrays.fill(result, "");
         int part = (int) (Math.floor(size / parts) + size % parts);
         int index;
+        StringBuffer stringBuffer = new StringBuffer();
         for (index = 0; index < part; index++) {
-            result[0] += matrix.getColumn(index).toString();
+            stringBuffer.append(matrix.getColumn(index).toString());
         }
+        result[0] = stringBuffer.toString();
 
         int preIndex = part;
         int elseParts = 0;
@@ -26,13 +28,15 @@ public class MatrixColumnSplitter <Value extends Matrix> implements DataSplitter
             elseParts = (size - part) / (parts - 1);
         }
         for (int j = 1; j < parts; j++) {
-            for (;index < size; index++) {
+            stringBuffer = new StringBuffer();
+            for (; index < size; index++) {
                 if (index - preIndex == elseParts) {
                     preIndex = index;
                     break;
                 }
-                result[j] += matrix.getColumn(index).toString();
+                stringBuffer.append(matrix.getColumn(index).toString());
             }
+            result[j] = stringBuffer.toString();
         }
         return result;
     }
